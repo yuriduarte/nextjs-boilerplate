c599d50516bff5677f742dcebf8e6a2dc9665c39
Yuri Duarte <yuriduartedev@gmail.com>
2020-09-15 16:50:20 -0300

    Configurando storybook v6 no projeto

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,4 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    '@storybook/addon-knobs'
-  ]
+  "stories": ["../src/components/**/stories.tsx"],
+  "addons": ["@storybook/addon-essentials"]
 }
