296dbc306e518258f7fd61212234fb1c00748d16
Yuri Duarte <yuriduartedev@gmail.com>
2020-09-15 12:12:10 -0300

    Criando estilos do primeiro componente

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
+import { Wrapper, Description, Logo, Title, Illustration } from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Wrapper>
+    <Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <Title>React Avançado</Title>
+    <Description>TypeScript, ReactJS, NextJS e Styled Components</Description>
+    <Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </Wrapper>
 );
 
 export default Main;
