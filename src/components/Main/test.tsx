ddbeea3005fe2f700f28715c862d41c3b2d0f08e
Yuri Duarte <yuriduartedev@gmail.com>
2020-09-15 17:37:38 -0300

    Adicionando PWA ao projeto

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -13,9 +13,9 @@ describe('<Main />', () => {
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  // it('should render the colors correctly', () => {
-  //   const { container } = render(<Main />);
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
 
-  //   expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
-  // });
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
+  });
 });
